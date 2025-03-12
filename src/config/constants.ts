ae9d5e54c6b7adba2e1ca39b8729da20feaed696
tylerjusfly <tylerjusfly1@gmail.com>
2025-03-12 17:28:47 +0100

    feat: completed seeding database implementation

---
+++ src/config/constants.ts
@@ -0,0 +1,3 @@
+export const imageSources = {
+    classifiedPlaceholder: "https://www.bmwusa.com/content/dam/bmw/common/limited-edition/x5-anniversary/BMW-SOC25-X5Silver-LimitedEdition-all-v2.jpg.bmwimg.xlarge.jpg"
+}
